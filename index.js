26a0196f5cd0d8b3b1ae49540a0cb33b32a49c45
DanielJoel <danpersie53@gmail.com>
2024-03-30 23:32:57 +0300

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let arrMessages = ["Saka", "Odegaard", "Jesus"]
+
+function writeCards(arr, event) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+
+}
+
+function countDown(integer) {
+    let i = integer
+    while (i >= 0) { console.log(i--) }
+}
